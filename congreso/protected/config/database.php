7d090a911c560d4e0ff959668e2e616304ce5236
unknown <lo-w-rider@hotmail.com>
2015-04-16 10:05:39 -0500

    conexion BD

--- congreso/protected/config/database.php
+++ congreso/protected/config/database.php
@@ -5,10 +5,10 @@ return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	
-	'connectionString' => 'mysql:host=localhost;dbname=bdcitec',
+	'connectionString' => 'mysql:host=localhost;dbname=equipo3',
 	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '',
+	'username' => 'equipo3',
+	'password' => 'iouuo7r3',
 	'charset' => 'utf8',
 	
 );
